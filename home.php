ffa7811973eed4757f667fcf83520bbcaaeefd71
Prakash Poudel <prakash.poudel@vecna.com>
2013-07-07 16:48:18 -0400

    Added new files

---
+++ home.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="style/style.css" type="text/css">
+    <link rel="icon" href="img/favicon.ico">
+    <title>Shopping Department</title>
+  </head>
+  <body>
+    <div class="header"> Header</div>
+  	<div class="mainContent">
+  	  <div class="cleardiv"></div>
+  	  <div class="innerContent">
+  	    <div class="leftContent">
+  	    
+  	    
+  	    </div>
+  	    <div class="centerContent">
+  	    
+  	    </div>
+  	    <div class="rightContent">
+  	      <div class="imageContent"></div>
+  	    </div>
+  	    
+  	  </div>
+  	</div>
+  
+  </body>
+</html>
+
