4e5068fdd93d656f262d48f295ac9baa178e39c8
Prakash Poudel <prakash.poudel@clinicalink.com>
2016-06-26 10:30:41 -0400

    Updated UI

---
+++ web/js/commentBox.js
@@ -0,0 +1,15 @@
+// tutorial1.js
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+<CommentList />
+      </div>
+    );
+  }
+});
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('leftContent')
+);
