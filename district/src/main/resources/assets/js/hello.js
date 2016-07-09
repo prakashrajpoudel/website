8feb22d80315ccbbb605be738153200e049d06e4
Prakash <prakashrajpoudel@gmail.com>
2016-07-09 10:40:27 -0400

    Removed http

--- district/src/main/resources/assets/js/hello.js
+++ district/src/main/resources/assets/js/hello.js
@@ -1,5 +1,5 @@
 function Hello($scope, $http) {
-$http.get('http://localhost:8080/api/experiment').
+$http.get('//localhost:8080/api/experiment').
   success(function(data) {
       console.log("test Server data");
       $scope.greeting = data;
