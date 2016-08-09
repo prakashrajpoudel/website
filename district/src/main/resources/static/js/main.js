a59679d83be7e5b84bc1257c651b2aaa2d545818
Prakash Poudel <prakash.poudel@clinicalink.com>
2016-08-09 12:15:14 -0400

    Added menu items

--- district/src/main/resources/static/js/main.js
+++ district/src/main/resources/static/js/main.js
@@ -1,8 +1,20 @@
-var main = angular.module('main', [ 'ngRoute', 'login', 'residence', 'welcome', 'customGrid' ]).config(
+var main = angular.module('main',
+		[ 'ngRoute', 'login', 'residence', 'welcome', 'customGrid' ]).config(
 		function($routeProvider) {
 			$routeProvider
 			// route for the contact page
 			// .when('/', {
 			// redirectTo : '/'
-			//			});
+			// });
 		});
+main.controller("menuController", function($scope) {
+	$scope.expandMenu = function() {
+		// Change to Jquery
+		var x = document.getElementById("myTopnav");
+		if (x.className === "topnav") {
+			x.className += " responsive";
+		} else {
+			x.className = "topnav";
+		}
+	};
+});
