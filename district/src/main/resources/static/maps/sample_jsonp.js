2f3a3ea2a790d1b9e357ef4d32e71743e54d9c48
Prakash Poudel <prakash.poudel@clinicalink.com>
2016-08-29 11:02:19 -0400

    Added google maps

---
+++ district/src/main/resources/static/maps/sample_jsonp.js
@@ -0,0 +1,20 @@
+eqfeed_callback({  
+   "features":[  
+      {  
+         "type":"Feature",
+         "geometry":{  "type":"Point",
+            "coordinates":[  
+               126.3832,
+               5.9775
+            ]  },
+         "id":"usc000csx3"
+      },
+{ 
+         "type":"Feature",
+         "geometry":{ "type":"Point",
+            "coordinates":[  
+               -178.3725,
+               -20.753
+            ] },
+         "id":"usc000csw5"
+      }]})
